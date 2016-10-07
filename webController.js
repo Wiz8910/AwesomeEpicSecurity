29ba7cfc250eded4c4325ff4598d97b710922f89
Wiz8910 <adamconaldbowers@gmail.com>
2016-10-07 10:23:41 -0500

    Very rough website outline

---
+++ webController.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+//here is the encrypt function
+document.getElementById("Encrypt").onclick = function(){
+	console.log("hello");
+};
+
+//here is the decrypt function
+document.getElementById("Decrypt").onclick = function(){
+	console.log("hlo");
+};
